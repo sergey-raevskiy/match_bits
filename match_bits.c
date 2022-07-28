877842752c06d5520b839d3ff2c1b1160ec87147
sergey-raevskiy <ccik@inbox.ru>
2022-07-28 18:02:25 +0300

    style.

--- match_bits.c
+++ match_bits.c
@@ -1,6 +1,6 @@
 #include "match_bits.h"
 
-int match_bits(match_bits_t* m, const void* d, size_t l, const char* p)
+int match_bits(match_bits_t *m, const void* d, size_t l, const char* p)
 {
     return 0;
 }
