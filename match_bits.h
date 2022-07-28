ffd73bbb2de820822386eb10712884da390f4e95
sergey-raevskiy <ccik@inbox.ru>
2022-07-28 18:20:26 +0300

    style.

--- match_bits.h
+++ match_bits.h
@@ -6,4 +6,4 @@ typedef struct {
     uint64_t a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
 } match_bits_t;
 
-int match_bits(match_bits_t *m, const void* d, size_t l, const char* p);
+int match_bits(match_bits_t *m, const void *d, size_t l, const char *p);
